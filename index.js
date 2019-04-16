f78e685cb9abc343740ef8e17f248fbd076f6a21
Addie Dean <addiedean@gmail.com>
2019-04-16 17:48:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
- var removeLastKitten=kittens.slice('Garfield');
+ var removeLastKitten=kittens.slice(3);
   return removeLastKitten;
 }
